aeaa5d76097ce3386b94daaadcd52df301d1f231
Kat Buchanan <katrbuch@gmail.com>
2021-04-10 15:28:34 -0500

    required routes

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false

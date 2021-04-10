b64f539d8f421502ad12385452ab0e624ec12c11
Kat Buchanan <katrbuch@gmail.com>
2021-04-10 17:00:44 -0500

    fixed paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,13 @@
 const path = require('path');
 
 module.exports = (app) => {
-    
-    app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+    app.get('/exercise', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/exercise.html'));
     });
 
     app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
+        res.sendFile(path.join(__dirname, './public/stats.html'));
     });
 
 }

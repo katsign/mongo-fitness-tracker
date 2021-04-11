cd62663ea14d7f04dd0e52b8c073f6ca0d67716d
Kat Buchanan <katrbuch@gmail.com>
2021-04-10 21:18:36 -0500

    really fixed paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,16 @@ const path = require('path');
 
 module.exports = (app) => {
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve('public/index.html'));
+    });
+
     app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/exercise.html'));
+        res.sendFile(path.resolve('public/exercise.html'));
     });
 
     app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, './public/stats.html'));
+        res.sendFile(path.resolve('public/stats.html'))
     });
 
 }

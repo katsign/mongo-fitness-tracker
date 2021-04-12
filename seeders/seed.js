c127c044144ef375a4597f24c68926b502cf2832
Kat Buchanan <katrbuch@gmail.com>
2021-04-12 17:55:13 -0500

    updating connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,8 +1,10 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
